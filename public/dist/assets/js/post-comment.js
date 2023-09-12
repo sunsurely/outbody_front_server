c060739ed3ac8c7d2efb08c7701bdcc592ea5478
Kim Pillseon <pillsun00@naver.com>
2023-09-12 15:08:11 +0900

    fix 댓글쪽 프로필 사진 모양 크기 원형

--- public/dist/assets/js/post-comment.js
+++ public/dist/assets/js/post-comment.js
@@ -32,7 +32,7 @@ const getOnePost = async () => {
     let temphtml = `<div class="card-header">
                       <ul class="list-unstyled user-details list-unstyled-border list-unstyled-noborder">
                         <li class="media">
-                          <img alt="image"  style="border-radius:50%; width:50px; height:50px
+                          <img alt="image"  style="border-radius:50%; width:50px; height:50px"
                             src="${profileImage}">
                           <div class="media-body">
                             <div class="media-title">${post.userName}</div>
