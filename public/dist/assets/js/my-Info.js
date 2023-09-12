4f674f17617879eedaee8de41a691bbed79a9af3
Kim Pillseon <pillsun00@naver.com>
2023-09-12 23:47:36 +0900

    fix 친구찾기 버튼 수리 완료

--- public/dist/assets/js/my-Info.js
+++ public/dist/assets/js/my-Info.js
@@ -332,7 +332,6 @@ $(signoutBtn).click(async () => {
 
 //친구찾기 , 친구요청 보내기
 $('#searchFriendByEmail').on('click', async () => {
-  console.log('이건 또 왜안되는데?');
   const email = $('#searchEmail').val();
   const searchUser = $('#searched-friend');
   $(searchUser).html('');
