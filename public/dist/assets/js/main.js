e3134fd3d51ede1dbeecffe51a8fb2fb105315d8
Kim Pillseon <pillsun00@naver.com>
2023-09-12 23:13:06 +0900

    fix 체중 차트 스텝 5로 변경

--- public/dist/assets/js/main.js
+++ public/dist/assets/js/main.js
@@ -86,7 +86,7 @@ async function initializeChart() {
   );
 
   initChart('myChart', bmrArr, dateArr, 50, '기초대사량(kcal)');
-  initChart('myChart2', weightArr, dateArr, 1, '체중(kg)');
+  initChart('myChart2', weightArr, dateArr, 5, '체중(kg)');
   initChart('myChart3', muscleArr, dateArr, 5, '골격근량(kg)');
   initChart('myChart4', fatArr, dateArr, 5, '체지방률(%)');
 }
